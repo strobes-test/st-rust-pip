680338506408e7747a64400a1b9e437a0235c3a1
avinashbakshi01 <avinash@wesecureapp.com>
2023-03-22 23:42:51 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# rust-pip
+
+(We don't have a logo yet so anything is welcomed)
+
+WORK IN PROGRESS
+
+Pip rewritten in Rust.
+
+## Features
+
+* TODO
+
+## How to contribute
+
+Please read this file and follow its instructions when it comes to
+contributing ot the project:
+
+* [CONTRIBUTING.md](CONTRIBUTING.md)
+
+## Credits
+
+This package was created with Cookiecutter, and the
+`John15321/cookiecutter-krabby-patty` project template.
+
+Cookiecutter: <https://github.com/audreyr/cookiecutter>
+
+`John15321/cookiecutter-krabby-patty`: <https://github.com/John15321/cookiecutter-krabby-patty>
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use clap::{AppSettings, Parser};
+
+/// Python package manager written in Rust
+#[derive(Parser, Debug)]
+#[clap(global_setting = AppSettings::DeriveDisplayOrder)]
+enum Opt {
+    /// Install packages.
+    Install {},
+    /// Download packages.
+    Download {
+        #[clap(short = 'n', long = "name")]
+        name: String,
+        #[clap(short = 'i', long = "index", default_value = "https://pypi.org/")]
+        index: String,
+    },
+    /// Uninstall packages.
+    Uninstall {},
+    /// List installed packages.
+    List {},
+    /// Show information about installed packages.
+    Show {},
+    /// Output installed packages in requirements format.
+    Freeze {},
+    /// Verify installed packages have compatible dependencies.
+    Check {},
+    /// Manage local and global configuration.
+    Config {},
+    /// Search PyPI for packages.
+    Search {},
+    /// Inspect and manage pip's wheel cache.
+    Cache {},
+    /// Inspect information available from package indexes.
+    Index {},
+    /// Build wheels from your requirements.
+    Wheel {},
+    /// Compute hashes of package archives.
+    Hash {},
+    /// A helper command used for command completion.
+    Completion {},
+    /// Show information useful for debugging.
+    Debug {},
+    /// Show help for commands.
+    Help {},
+}
+
+fn download_package(_package_name: String, _package_index: &str) {}
+
+fn main() {
+    let opt = Opt::parse();
+    println!("{:#?}", opt);
+
+    match opt {
+        Opt::Download { name, index } => {
+            println!("Package name {:?}", name);
+            println!("Index name: {:?}", index);
+            download_package(name, &index);
+        }
+        _ => todo!(),
+    }
+}
